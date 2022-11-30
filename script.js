84919128d9913e63c66ea88650b3097df1465335
ChauBui00 <c.bui3@share.epsb.ca>
2022-11-30 11:03:29 -0700

    edit

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// Script.js
+// Story of Gauss
+
+// HTML Elemets
+let calcBtn = document.getElementById("calc-btn");
+
+
+// Add event Listener
+calcBtn.addEventListener("click", calcSum);
+
+// Event Function
+function calcSum() {
+    // Print intergers 1 to 100
+    for (let n = 1; n <= 100; n++) {
+        console.log(n);
+        
+    }
+}
